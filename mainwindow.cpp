35eb2aa41e876f1d7187f348b233b747a0107861
gabriel <gabrielbchuede@gmail.com>
2023-10-30 22:42:26 -0300

    Limpeza comentarios e estruturas inúteis.
    Comentários chatgpt, estruturas de teste para achar o erro anterior,
    insercao e leitura dos dados do db.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,22 +18,12 @@ MainWindow::MainWindow(QWidget *parent)
     if(!db.open()){
         qDebug() << "erro ao abrir banco de dados.";
     }
-
-    // verificar se tabela ja existe
     QSqlQuery query;
-    query.exec("SELECT name FROM sqlite_master WHERE type='table' AND name='produtos'");
-
-    if (query.next()) {
-        qDebug() << "a tabela produtos já existe.";
-    }
-    else {
-        QSqlQuery query;
-        query.exec("CREATE TABLE produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, quantidade INTEGER, descricao TEXT)");
-        if (query.isActive()) {
-            qDebug() << "Tabela criada com sucesso!";
-        } else {
-            qDebug() << "Erro ao criar tabela: ";
-        }
+    query.exec("CREATE TABLE produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, quantidade INTEGER, descricao TEXT)");
+    if (query.isActive()) {
+        qDebug() << "Tabela criada com sucesso!";
+    } else {
+        qDebug() << "Erro ao criar tabela: ";
     }
     qDebug() << db.tables();
 
@@ -42,7 +32,6 @@ MainWindow::MainWindow(QWidget *parent)
     model->setQuery("SELECT * FROM produtos");
     ui->Tview_Produtos->setModel(model);
     QSqlDatabase::database().close();
-//    ui->Tview_Produtos->show();
 }
 
 MainWindow::~MainWindow()
@@ -56,51 +45,21 @@ void MainWindow::on_Btn_Enviar_clicked()
     nomeProduto = ui->Ledit_Nome->text();
     quantidadeProduto = ui->Ledit_Quantidade->text();
     descProduto = ui->Ledit_Desc->text();
-//    Produto addProduto(nomeProduto, descProduto, quantidadeProduto.toInt());
-//    produtos.push_back(addProduto);
-//    registro = nomeProduto + "," + quantidadeProduto + "," + descProduto + "\n";
-//    QStandardItem *newNome = new QStandardItem(nomeProduto);
-//    QStandardItem *newQuantidade = new QStandardItem(quantidadeProduto);
-//    QStandardItem *newDesc = new QStandardItem(descProduto);
-//    rowCount = model->rowCount();
-//    model->setItem(rowCount, 0, newNome);
-//    model->setItem(rowCount, 1, newQuantidade);
-//    model->setItem(rowCount, 2, newDesc);
-//    QFile arquivo("../QEstoqueLoja/estoque.txt");
-//    if (arquivo.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
-//        // Crie um objeto QTextStream para escrever no arquivo
-//        QTextStream out(&arquivo);
-//        // Escreva os dados no arquivo
-//        out << registro;
-//        // Feche o arquivo
-//        arquivo.flush();
-//        arquivo.close();
-//        ui->Ledit_Desc->clear();
-//        ui->Ledit_Nome->clear();
-//        ui->Ledit_Quantidade->clear();
-//        ui->Ledit_Nome->setFocus();
-//    } else {
-//        QMessageBox::warning(this,"ERRO", "Algo deu errado ao escrever no arquivo.");
-//    }
+    // adicionar ao banco de dados
     if(!db.open()){
         qDebug() << "erro ao abrir banco de dados. botao enviar.";
     }
     QSqlQuery query;
 
-    // Definir o comando SQL de inserção
-    QString insertSql = "INSERT INTO produtos (nome, quantidade, descricao) VALUES (:valor2, :valor3, :valor4)";
-    query.prepare(insertSql);
-
-    // Atribuir valores aos parâmetros
-    query.bindValue(":valor2", nomeProduto);
-    query.bindValue(":valor3", quantidadeProduto);
-    query.bindValue(":valor4", descProduto);
-
-    // Executar a consulta
+    query.prepare("INSERT INTO produtos (nome, quantidade, descricao) VALUES (:valor1, :valor2, :valor3)");
+    query.bindValue(":valor1", nomeProduto);
+    query.bindValue(":valor2", quantidadeProduto);
+    query.bindValue(":valor3", descProduto);
     if (query.exec()) {
         qDebug() << "Inserção bem-sucedida!";
     } else {
         qDebug() << "Erro na inserção: ";
     }
+    QSqlDatabase::database().close();
 }
 

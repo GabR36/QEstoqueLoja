440276ccf1df6aca7fb3ffc15b85af9c331ef8de
gabriel <gabrielbchuede@gmail.com>
2023-11-23 18:14:45 -0300

    mostrar table view produtos_vendidos.

--- vendas.cpp
+++ vendas.cpp
@@ -10,7 +10,8 @@ Vendas::Vendas(QWidget *parent) :
     QSqlQueryModel *modelo = new QSqlQueryModel;
     modelo->setQuery("SELECT * FROM vendas2");
     ui->Tview_Vendas2->setModel(modelo);
-//    modelo->setQuery("SELECT * FROM ");
+    modelo->setQuery("SELECT * FROM produtos_vendidos");
+    ui->Tview_ProdutosVendidos->setModel(modelo);
 }
 
 Vendas::~Vendas()

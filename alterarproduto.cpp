b39e729a9be2811ee93db032bd17cfd0a3cd7426
gabriel <gabrielbchuede@gmail.com>
2023-12-12 18:23:27 -0300

    Impedir preço invalido, alterarproduto

--- alterarproduto.cpp
+++ alterarproduto.cpp
@@ -2,6 +2,7 @@
 #include "ui_alterarproduto.h"
 #include "mainwindow.h"
 #include "QSqlQuery"
+#include <QMessageBox>
 
 AlterarProduto::AlterarProduto(QWidget *parent) :
     QDialog(parent),
@@ -27,24 +28,43 @@ void AlterarProduto::on_Btn_AltAceitar_accepted()
     QString quant = ui->Ledit_AltQuant->text();
     QString preco = ui->Ledit_AltPreco->text();
 
-    // alterar banco de dados
-    if(!janelaPrincipal->db.open()){
-        qDebug() << "erro ao abrir banco de dados. botao alterar->aceitar.";
-    }
-    QSqlQuery query;
+    // Substitua ',' por '.' se necessário
+    preco.replace(',', '.');
 
-    query.prepare("UPDATE produtos SET quantidade = :valor2, descricao = :valor3, preco = :valor4 WHERE id = :valor1");
-    query.bindValue(":valor1", idAlt);
-    query.bindValue(":valor2", quant);
-    query.bindValue(":valor3", desc);
-    query.bindValue(":valor4", preco);
-    if (query.exec()) {
-        qDebug() << "Alteracao bem-sucedida!";
-    } else {
-        qDebug() << "Erro na alteracao: ";
+    // Converta o texto para um número
+    bool conversionOk;
+    double price = preco.toDouble(&conversionOk);
+
+    // Verifique se a conversão foi bem-sucedida e se o preço é maior que zero
+    if (conversionOk && price >= 0)
+    {
+        // Armazene o preço em uma variável ou faça o que precisar com ele
+        // Neste exemplo, apenas exibimos uma mensagem
+        QMessageBox::information(this, "Sucesso", "Preço válido: " + QString::number(price));
+        // alterar banco de dados
+        if(!janelaPrincipal->db.open()){
+            qDebug() << "erro ao abrir banco de dados. botao alterar->aceitar.";
+        }
+        QSqlQuery query;
+
+        query.prepare("UPDATE produtos SET quantidade = :valor2, descricao = :valor3, preco = :valor4 WHERE id = :valor1");
+        query.bindValue(":valor1", idAlt);
+        query.bindValue(":valor2", quant);
+        query.bindValue(":valor3", desc);
+        query.bindValue(":valor4", preco);
+        if (query.exec()) {
+            qDebug() << "Alteracao bem-sucedida!";
+        } else {
+            qDebug() << "Erro na alteracao: ";
+        }
+        // mostrar na tableview
+        janelaPrincipal->atualizarTableview();
+        QSqlDatabase::database().close();
     }
-    // mostrar na tableview
-    janelaPrincipal->atualizarTableview();
-    QSqlDatabase::database().close();
+    else
+    {
+        // Exiba uma mensagem de erro se o preço não for válido
+        QMessageBox::warning(this, "Erro", "Por favor, insira um preço válido.");
+    }
 }
 

b11028ef6cdce55a45d0623e516ffd0af30aaca1
gabriel <gabrielbchuede@gmail.com>
2024-03-01 13:38:10 -0300

    validação preços e quantidades venda nova

--- venda.cpp
+++ venda.cpp
@@ -192,29 +192,6 @@ void venda::on_Ledit_Barras_returnPressed()
 
 void venda::on_Btn_Aceitar_clicked()
 {
-    // inserir a venda
-    QString cliente = ui->Ledit_Cliente->text();
-
-    // adicionar ao banco de dados
-    if(!db.open()){
-        qDebug() << "erro ao abrir banco de dados. botao aceitar venda.";
-    }
-    QSqlQuery query;
-
-    query.prepare("INSERT INTO vendas2 (cliente, total, data_hora) VALUES (:valor1, :valor2, :valor3)");
-    query.bindValue(":valor1", cliente);
-    query.bindValue(":valor2", Total());
-    // inserir a data do dateedit
-    query.bindValue(":valor3", ui->DateEdt_Venda->dateTime().toString("dd-MM-yyyy HH:mm:ss"));
-    QString idVenda;
-    if (query.exec()) {
-        idVenda = query.lastInsertId().toString();
-        qDebug() << "Inserção bem-sucedida!";
-    } else {
-        qDebug() << "Erro na inserção: ";
-    }
-    // inserir os produtos da venda
-
     // pegar os valores da tabela dos produtos selecionados
     QList<QList<QVariant>> rowDataList;
     for (int row = 0; row < modeloSelecionados.rowCount(); ++row){
@@ -226,32 +203,83 @@ void venda::on_Btn_Aceitar_clicked()
         rowDataList.append(rowData);
     }
     qDebug() << rowDataList;
-
-    // adicionar ao banco de dados
+    // validar preço e quantidade
+    bool erro = false;
     for (const QList<QVariant> &rowdata : rowDataList) {
-        query.prepare("INSERT INTO produtos_vendidos (id_produto, quantidade, preco_vendido, id_venda) VALUES (:valor1, :valor2, :valor3, :valor4)");
-        query.bindValue(":valor1", rowdata[0]);
-        query.bindValue(":valor2", rowdata[1]);
-        query.bindValue(":valor3", rowdata[3]);
-        query.bindValue(":valor4", idVenda);
-        if (query.exec()) {
-            qDebug() << "Inserção prod_vendidos bem-sucedida!";
-        } else {
-            qDebug() << "Erro na inserção prod_vendidos: ";
-        }
-        query.prepare("UPDATE produtos SET quantidade = quantidade - :valor2 WHERE id = :valor1");
-        query.bindValue(":valor1", rowdata[0]);
-        query.bindValue(":valor2", rowdata[1]);
-        if (query.exec()) {
-            qDebug() << "update quantidade bem-sucedida!";
-        } else {
-            qDebug() << "Erro na update quantidade: ";
+        // Substitua ',' por '.' se necessário
+        QString preco = rowdata[3].toString();
+        QString quant = rowdata[1].toString();
+        preco.replace(',', '.');
+
+        // Converta o texto para um número
+        bool conversionOk, conversionOkQuant;
+        double price = preco.toDouble(&conversionOk);
+        int quantINT = quant.toInt(&conversionOkQuant);
+
+        // Verifique se a conversão foi bem-sucedida e se o preço é maior que zero
+        if (!(conversionOk && price >= 0) || !(conversionOkQuant && quantINT > 0))
+        {
+            erro = true;
         }
     }
-    db.close();
-    janelaVenda->atualizarTabelas();
-    janelaPrincipal->atualizarTableview();
-    this->close();
+    qDebug() << "Erro = ";
+    qDebug() << erro;
+    if (!erro){
+        // se nao tiver erro na validaçao, prossiga
+        // inserir a venda
+        QString cliente = ui->Ledit_Cliente->text();
+
+        // adicionar ao banco de dados
+        if(!db.open()){
+            qDebug() << "erro ao abrir banco de dados. botao aceitar venda.";
+        }
+        QSqlQuery query;
+
+        query.prepare("INSERT INTO vendas2 (cliente, total, data_hora) VALUES (:valor1, :valor2, :valor3)");
+        query.bindValue(":valor1", cliente);
+        query.bindValue(":valor2", Total());
+        // inserir a data do dateedit
+        query.bindValue(":valor3", ui->DateEdt_Venda->dateTime().toString("dd-MM-yyyy HH:mm:ss"));
+        QString idVenda;
+        if (query.exec()) {
+            idVenda = query.lastInsertId().toString();
+            qDebug() << "Inserção bem-sucedida!";
+        } else {
+            qDebug() << "Erro na inserção: ";
+        }
+        // inserir os produtos da venda
+
+        // adicionar ao banco de dados
+        for (const QList<QVariant> &rowdata : rowDataList) {
+            query.prepare("INSERT INTO produtos_vendidos (id_produto, quantidade, preco_vendido, id_venda) VALUES (:valor1, :valor2, :valor3, :valor4)");
+            query.bindValue(":valor1", rowdata[0]);
+            query.bindValue(":valor2", rowdata[1]);
+            query.bindValue(":valor3", rowdata[3]);
+            query.bindValue(":valor4", idVenda);
+            if (query.exec()) {
+                qDebug() << "Inserção prod_vendidos bem-sucedida!";
+            } else {
+                qDebug() << "Erro na inserção prod_vendidos: ";
+            }
+            query.prepare("UPDATE produtos SET quantidade = quantidade - :valor2 WHERE id = :valor1");
+            query.bindValue(":valor1", rowdata[0]);
+            query.bindValue(":valor2", rowdata[1]);
+            if (query.exec()) {
+                qDebug() << "update quantidade bem-sucedida!";
+            } else {
+                qDebug() << "Erro na update quantidade: ";
+            }
+        }
+        db.close();
+        janelaVenda->atualizarTabelas();
+        janelaPrincipal->atualizarTableview();
+        this->close();
+    }
+    else {
+        // Exiba uma mensagem de erro se o preço ou a quantidade não for válido
+        QMessageBox::warning(this, "Erro", "Por favor, insira preços e/ou quantidades válidas.");
+    }
+
 }
 
 QString venda::Total(){

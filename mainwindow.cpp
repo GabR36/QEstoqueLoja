55981e6a2d221eee8c502cae7949d213a2d9bbeb
RafaelBormannChuede <rafaelchuede@gmail.com>
2024-06-20 12:47:16 -0300

    arrumado pesquisa por codigo de barra apenas quando 1 palavra é digitada

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -418,7 +418,7 @@ void MainWindow::on_Btn_Pesquisa_clicked()
     QStringList conditions;
     if (palavras.length() > 1){
         for (const QString &palavra : palavras) {
-            conditions << QString("descricao LIKE '%%1%' OR codigo_barras LIKE '%%1%'").arg(palavra);
+            conditions << QString("descricao LIKE '%%1%'").arg(palavra);
 
         }
 

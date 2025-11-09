eea93e42fa10917667abfe74683334cb87ef97d4
Gabriel Chuede <gabrielbchuede@gmail.com>
2025-11-08 21:57:53 -0300

    Initial Debian packaging

---
+++ debian/upstream.ex/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/qestoqueloja/issues
+# Bug-Submit: https://github.com/<user>/qestoqueloja/issues/new
+# Changelog: https://github.com/<user>/qestoqueloja/blob/master/CHANGES
+# Documentation: https://github.com/<user>/qestoqueloja/wiki
+# Repository-Browse: https://github.com/<user>/qestoqueloja
+# Repository: https://github.com/<user>/qestoqueloja.git
